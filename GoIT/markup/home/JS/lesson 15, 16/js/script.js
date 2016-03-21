2ff333ecbb141c2508cce8b7553adee8d0a9ba25
Bidiuk <sanyabid@gmail.com>
2016-03-21 23:34:04 +0200

    up

---
+++ GoIT/markup/home/JS/lesson 15, 16/js/script.js
@@ -0,0 +1,35 @@
+function GoogleCallback(jobj, data) {
+    var ul = document.createElement("ul");
+    var results = $('.results');
+    $.each(data.results, function(i, val) {
+        var li = document.createElement("li");
+        li.innerHTML = '<a href="' + val.url + '"><h3 class="title">' + val.title + '</h3></a> <p class="visibleUrl">'+val.visibleUrl+'</p> <p class="content">'+val.content+'</p>';                        
+        ul.appendChild(li);
+        console.log(val);
+    });
+    results.html(ul);
+};
+
+$(function() {
+
+    function request() {
+        var text = $('#text').val();
+
+        $.ajax({
+            // AJAX-specified URL
+            url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=' + text + '&callback=GoogleCallback&context=?',
+            dataType: "jsonp",
+
+        });
+    };
+
+    $('.submit').on('click', request);
+
+    $('#text').keypress(function(event) {
+        if (event.which == 13) {
+            request();
+        }
+    });
+
+
+});
